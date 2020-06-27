e94b07af6abd09f6bdf09003e63ef78fba41a5a9
LTStorm <49832930+LTStorm@users.noreply.github.com>
2020-06-27 10:24:24 +0700

    Load image name

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
 app.use(express.static(path.join(__dirname,"..")));
 
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(directoryPath,(err,files) => {
+            if (err) {
+                return reject(err);
+            }
+            var list_image=[];
+            files.forEach((image)=>list_image.push(image));
+            return resolve(list_image);
+        });
+    });
+}
+
 app.get("/",(req,res)=>{
     filePath = path.join(__dirname,"..","index.html");
     res.sendFile(filePath);
