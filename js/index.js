01daf73c6ea41494547c03df29a1a3aafc92040c
Hoàng Trương <hoangtruong181099@gmail.com>
2020-07-04 00:23:32 +0700

    update event + merge 2 files

--- js/index.js
+++ js/index.js
@@ -6,22 +6,11 @@ async function getImg(){
     await fetch("/api")
     .then((res)=> res.json())
     .then(dat => {
-        let i=1;
       dat.forEach(e  => {
           arr.push(e);
-          
-          html += '<div class="col-lg-4 col-md-4 col-sm-6 col-6"><div class="gallery_pt"><img id= \'' +i+'\' src=\' '+ e + '\'</></div></div>'
-          i++;
+          html += '<div class="col-lg-4 col-md-4 col-sm-6 col-6"><div class="gallery_pt"><img src=\' '+ e + '\'</></div></div>'
       })
-    //   var j;
-    //   for(j=1;j<=dat.length;j++){
-    //         document.getElementById(j.toString()).addEventListener('click',function (e){
-                
-    //         });
-    //   }
       document.getElementById("img").innerHTML = html;
-      var a = document.getElementById('1');
-      console.log(a);
       return arr;
     })
 }
